c4452bb1c9fdc8705e9fcebd450bd2f3c955fac4
Ajwad <dev@ajwadtahmid.com>
2024-07-31 17:00:16 -0700

    added user routes and controller,  cloudinary for images

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -87,6 +87,7 @@ export const login = async (req, res) => {
 
 export const logout = async (req, res) => {
     try {
+        // reset cookie
         res.cookie("jwt","",{maxAge:0})
         res.status(200).json({message: "Logged out successfully"});
     } catch (error) {
